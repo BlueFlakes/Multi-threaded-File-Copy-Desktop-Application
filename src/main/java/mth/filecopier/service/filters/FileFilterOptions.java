84d3f7012f2525ebd83fa33345086c92d98540ee
Kamil <blueflamesxcode@gmail.com>
2018-03-01 14:18:09 +0100

    add option getter by identity and exception for it

--- src/main/java/mth/filecopier/service/filters/FileFilterOptions.java
+++ src/main/java/mth/filecopier/service/filters/FileFilterOptions.java
@@ -1,6 +1,21 @@
 package mth.filecopier.service.filters;
 
+import mth.filecopier.exceptions.InvalidChoiceException;
+
 public enum FileFilterOptions {
     NO_FILTER,
-    IGNORE_DUPLICATES_FILTER
+    IGNORE_DUPLICATES_FILTER;
+
+    public static FileFilterOptions getOptionByIdentity(String identity)
+            throws InvalidChoiceException {
+
+        if (identity.equals("NO")) {
+            return IGNORE_DUPLICATES_FILTER;
+
+        } else if (identity.equals("YES")) {
+            return NO_FILTER;
+        }
+
+        throw new InvalidChoiceException("Invalid identity: " + identity);
+    }
 }
