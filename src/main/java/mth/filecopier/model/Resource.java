58404e7454c6bb2f1550b35fdc046c035d78f810
Val1706 <potapovvalentine@gmail.com>
2018-03-01 13:50:23 +0100

    make shorter connection between creating File inside resoource

--- src/main/java/mth/filecopier/model/Resource.java
+++ src/main/java/mth/filecopier/model/Resource.java
@@ -10,9 +10,9 @@ public class Resource {
     private File source;
     private File destination;
 
-    public Resource(File source, File destination) {
-        this.source = source;
-        this.destination = destination;
+    public Resource(String sourcePath, String destination) {
+        this.source = new File(sourcePath);
+        this.destination = new File(sourcePath);
     }
 
     public File getSource( ) {
