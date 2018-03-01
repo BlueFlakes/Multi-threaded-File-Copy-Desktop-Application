5f4e04ad5e5276abca0503f22c4a8e0604ff93ec
Kamil <blueflamesxcode@gmail.com>
2018-03-01 14:22:25 +0100

    add default constructor

--- src/main/java/mth/filecopier/exceptions/ApplicationException.java
+++ src/main/java/mth/filecopier/exceptions/ApplicationException.java
@@ -1,6 +1,9 @@
 package mth.filecopier.exceptions;
 
 public abstract class ApplicationException extends Exception {
+
+    public ApplicationException() {}
+
     public ApplicationException(String s) {
         super(s);
     }
